3b8fe4a8c5ace93878af05c3034d893e144a1302
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-07-22 17:39:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = 21484575
-API_HASH = "db6286a041a873eb6c571df07ef4244e"
-BOT_TOKEN = "7385210085:AAGWZ3n2Vvs2VTjtwguEl-r6wQ25gjMBZ8I"
-MONGO_DB_URI = "mongodb+srv://DanishDBINN:DanishDBINN@cluster0.1ayw4i7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+API_ID = 
+API_HASH = ""
+BOT_TOKEN = ""
+MONGO_DB_URI = ""
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
-LOG_GROUP_ID = -1001861619812
-OWNER_ID = 6736679432
+LOG_GROUP_ID = 
+OWNER_ID = 
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -29,8 +29,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/learning_bots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/learning_bots")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -52,7 +52,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "BQFH1B8AYGfPX5FuKT89XiKLkN9DgSYfjeYaSz1LzIV2_78DAPCwkqa9l_GsBr7K0E-uw9emN91ECo2VBP2GOSyjy02GQgZ-pmzIJUt1GwIrSNKUAX93lSKwaiEYgx3J6ZvuGfDgWlPi2EkK6bZg4B3bUPc7gN-Eq9f1HDCOgEirvCLpSSm7H7JfTRvlzUkcA78d9Qje4WM4aGZ6Rv46y05k7zmY_q19EtgmAWlTYi5byxIE7c2od-v2P9bqEh8FZtkUqGvT9Ay0ixUjI3IiZrZa36mpP2wTJDByg3fNQJ51jHD8AysKealRF6s_4ooRTkUfvjMA-3hFy0z3F3GAzXukBYHWqAAAAAGRiZIIAA"
+STRING1 = ""
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
