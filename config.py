67aa4604b619d9bda894f1dd9057af8803035fba
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-07-31 20:51:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/timepassmarkus04/Kevin11",
+    "https://github.com/timepassmarkus04/Amritha2.0",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
